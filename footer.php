36822578d3491c3fa65cacdbe16ae72c3d763f9c
HighdraSC2 <davey.smeekens23@outlook.com>
2022-05-25 11:01:42 +0200

    Created footer page

---
+++ footer.php
@@ -0,0 +1,19 @@
+<footer>
+	<div class="wrapper">
+		<div class="footer-menu">
+			<img src="img/header_img.jpg" alt="">
+			<div id="current_date"></p>
+<script>
+document.getElementById("current_date").innerHTML = Date();
+</script>
+			<div class="adresgegevens">
+				<h4></h4>
+				<p>c2.bedrijf@gmail.com</p>
+			<div class=footernav>
+			<a href="contact.php">Neem contact op</a>
+			<a href="https://imdb.com">imdb.com</a>
+			<a href="https://videoland.nl">videoland.nl </a>
+			<a href="https://movies.nl">movies.nl</a>
+		</div>	
+	</div>	
+</footer>
