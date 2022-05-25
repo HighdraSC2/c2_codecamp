6ff116244bc586c0628247373a2d35bbb04a12e5
HighdraSC2 <davey.smeekens23@outlook.com>
2022-05-25 11:01:35 +0200

    Created header page

---
+++ header.php
@@ -0,0 +1,40 @@
+<head>
+  <meta charset="utf-8">
+  <title>C2</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <meta property="og:title" content="">
+  <meta property="og:type" content="">
+  <meta property="og:url" content="">
+  <meta property="og:image" content="">
+
+  <link rel="manifest" href="site.webmanifest">
+  <link rel="apple-touch-icon" href="icon.png">
+
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/main.css">
+
+  <meta name="theme-color" content="#fafafa">
+</head>
+<body>
+  <header>
+    <div class="wrapper">
+      <div class="header-menu">
+    		<div class="logo">
+    			<img src="img/header_img.jpg" alt="fantasy-header">	
+    			<h1>C2 &trade;</h1>
+    		</div>
+    		<nav>
+          <a href="index.php">Home</a>
+          <a href="songtekst.php">Songteksten</a>
+    			<a href="films.php">Films</a>
+    			<a href="contact.php">Contact</a>
+          <a href="tools.php">Tools</a>
+          <a href="books.php">Books</a>
+          <a href="actor.php">Actors</a>
+    		</nav>
+      </div>  
+   	</div>
+  </header>
+</body>
