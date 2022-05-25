2682a58e218767da37f86b378f719949da69ef24
HighdraSC2 <davey.smeekens23@outlook.com>
2022-05-25 11:02:35 +0200

    Added book page

---
+++ books.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Books</title>
+</head>
+<body>
+<?php 
+require_once("header.php");
+?>
+<main>
+	<div class="Wrapper">
+		<h4>Boeken Pagina </h4>
+		<div class="item">
+			<div class="film1">
+	  			<h3>Wat is een boek?</h3>
+	  			<img src="img/bibliotheek.jpeg" alt="item">
+	  			<p>Het boek is een veelgebruikte manier om informatie vast te leggen in de vorm van tekst en afbeeldingen en om deze informatie weer te geven, op te slaan en te verspreiden. Het is een document van vaak niet al te kleine omvang.
+                   Een boek in traditionele zin is een samengevoegde hoeveelheid papier met tekst, afbeeldingen of allebei. Daarnaast wordt met de term de betreffende informatie, of een soortgelijke verzameling informatie, aangeduid ('een boek schrijven', 'een boek downloaden').
+                   Het woord 'boek' is vermoedelijk afkomstig van het Germaanse woord voor beuk, boche. De omslagen van de eerste boeken werden namelijk gemaakt van een rechthoekig stuk (beuken)hout. Het Griekse woord biblia (boeken) is het meervoud van biblion dat oorspronkelijk 'papyrusje' betekende, het materiaal waarop in de oudheid meestal werd geschreven, en later werd gebruikt als aanduiding van papier, geschrift en boek.
+</p>
+
+</main>
+
+<?php
+require_once("footer.php");
+?>
+</body>
+</body>
+</html>
