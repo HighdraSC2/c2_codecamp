3f002c296ebe5153ea3b24814c7475ab7f0cab29
HighdraSC2 <davey.smeekens23@outlook.com>
2022-05-25 11:00:06 +0200

    Created songtekst page

---
+++ songtekst.php
@@ -0,0 +1,473 @@
+<body>
+
+  <!-- Add your site or application content here -->
+<?php 
+require_once("header.php");
+?>
+<main>
+  <div class="wrapper">
+  	<div class="shop-page">
+  		<h2>Songteksten</h2>
+  		<div class="shop">
+	  		<div class="item">
+	  			<h3>Madness</h3>
+	  			<img src="img/Madness.jpg" alt="item">
+				  <p><span>
+					I, I can't get these memories out of my mind
+					And some kind of madness has started to evolve
+					I, I tried so hard to let you go
+					But some kind of madness is swallowing me whole, yeah
+					I have finally seen the light
+					And I have finally realized
+					What you mean
+					Oh oh oh
+					And now, I need to know is this real love
+					Or is it just madness keeping us afloat?
+					And when I look back at all the crazy fights we had
+					Like some kind of madness
+					Was taking control			
+				</p>
+<button id="readMore">Lees meer</button><button id="readLess" style="display: none">Lees minder</button>
+	  			<p id="item1" style="display: none">Yeah
+					And now I have finally seen the light
+					And I have finally realized
+					What you need
+					Mm
+					But now I have finally seen the end (finally seen the end)
+					And I'm not expecting you to care (expecting you to care)
+					That I have finally seen the light (finally seen the light)
+					I have finally realized (realized)
+					I need to love
+					I need to love
+					Come to me
+					Trust in a dream
+					Come on and rescue me
+					Yes I know, I can be wrong
+					And maybe I'm too headstrong
+					Our love is
+					Madness
+				</p>
+
+	  		</div>
+	  		<div class="item">
+	  			<h3>Killer queen</h3>
+	  			<img src="img/queen.jpg" alt="item">
+	  			<p>
+					She keeps her Moët et Chandon
+					In her pretty cabinet
+					"Let them eat cake", she says
+					Just like Marie Antoinette
+					A built-in remedy
+					For Khrushchev and Kennedy
+					At anytime an invitation
+					You can't decline
+					Caviar and cigarettes
+					Well versed in etiquette
+					Extraordinarily nice
+					She's a Killer Queen
+					Gunpowder, gelatine
+					Dynamite with a laser beam
+					Guaranteed to blow your mind
+					Anytime
+				</p>
+	  			<button id="readMore2">Lees meer</button><button id="readLess2" style="display: none">Lees minder</button>
+	  			<p id="item2" style="display: none">
+					To avoid complications
+					She never kept the same address
+					In conversation
+					She spoke just like a baroness
+					Met a man from China
+					Went down to Geisha Minor
+					Then again incidentally
+					If you're that way inclined
+					Perfume came naturally from Paris (naturally)
+					For cars she couldn't care less
+					Fastidious and precise
+					She's a Killer Queen
+					Gunpowder, gelatine
+					Dynamite with a laser beam
+					Guaranteed to blow your mind
+					Anytime
+					Drop of a hat she's as willing as
+					Playful as a pussy cat
+					Then momentarily out of action
+					Temporarily out of gas
+					To absolutely drive you wild, wild
+					She's all out to get you
+					She's a Killer Queen
+					Gunpowder, gelatine
+					Dynamite with a laser beam
+					Guaranteed to blow your mind
+					Anytime
+					Recommended at the price
+					Insatiable an appetite
+					Wanna try?
+					You wanna try
+				</p>
+	  		</div>
+	  		<div class="item">
+	  			<h3>Bohemian Rhapsody</h3>
+	  			<img src="img/Bohemian.jpg" alt="item">
+	  			<p> 
+					Is this the real life?
+					Is this just fantasy?
+					Caught in a landside,
+					No escape from reality
+					Open your eyes,
+					Look up to the skies and see,
+					I'm just a poor boy, I need no sympathy,
+					Because I'm easy come, easy go,
+					Little high, little low,
+					Any way the wind blows doesn't really matter to
+					Me, to me
+					Mamaaa,
+					Just killed a man,
+					Put a gun against his head, pulled my trigger,
+					Now he's dead
+					Mamaaa, life had just begun,
+					But now I've gone and thrown it all away
+					..
+				</p>
+	  			<button id="readMore3">Lees meer</button><button id="readLess3" style="display: none">Lees minder</button>
+	  			<p id="item3" style="display: none">Mama, oooh,
+					Didn't mean to make you cry,
+					If I'm not back again this time tomorrow,
+					Carry on, carry on as if nothing really matters
+					Too late, my time has come,
+					Sends shivers down my spine, body's aching all
+					The time
+					Goodbye, everybody, I've got to go,
+					Gotta leave you all behind and face the truth
+					Mama, oooh
+					I don't want to die,
+					I sometimes wish I'd never been born at all.
+					I see a little silhouetto of a man,
+					Scaramouch, Scaramouch, will you do the Fandango!
+					Thunderbolts and lightning, very, very frightening me
+					Galileo, Galileo
+					Galileo, Galileo
+					Galileo, Figaro - magnificoo
+					I'm just a poor boy nobody loves me
+					He's just a poor boy from a poor family,
+					Spare him his life from this monstrosity
+					Easy come, easy go, will you let me go
+					Bismillah! No, we will not let you go
+					(Let him go!) Bismillah! We will not let you go
+					(Let him go!) Bismillah! We will not let you go
+					(Let me go) Will not let you go
+					(Let me go)(Never) Never let you go
+					(Let me go) (Never) let you go (Let me go) Ah
+					No, no, no, no, no, no, no
+					Oh mama mia, mama mia, mama mia, let me go
+					Beelzebub has a devil put aside for me, for me,
+					For meee
+					So you think you can stop me and spit in my eye
+					So you think you can love me and leave me to die
+					Oh, baby, can't do this to me, baby,
+					Just gotta get out, just gotta get right outta here
+					Nothing really matters, Anyone can see,
+					Nothing really matters,
+					Nothing really matters to me
+					Any way the wind blows...
+				</p>
+	  		</div>
+	  		<div class="item">
+	  			<h3>Uprising my baby</h3>
+	  			<img src="img/Uprising.jpg" alt="item">
+	  			<p>
+					Allow me in your tub
+					Just allow me in your club
+					Allow me to drink from your cup
+					I'll be coming back for more
+					Allow me in your dream
+					Allow me in your scene
+					Just allow me on your knees
+					I'll be coming back for more
+					Sisters, keep on rising
+					Brothers, keep on rising
+					This girl keeps on rising
+					Keep on rising up
+					All I Believe in your school...
+				  </p>
+	  			<button id="readMore4">Lees meer</button><button id="readLess4" style="display: none">Lees minder</button>
+	  			<p id="item4" style="display: none">I Believe in your rules
+					I Believe in your truth
+					I'll be coming back for more
+					I Believe in your land
+					I Believe in that man
+					I Believe in your command
+					I'll be coming back for more
+					Sisters, Brothers, Lovers
+					Keep on rising up
+					Sisters, keep on rising
+					Brothers, keep on rising
+					This girl keeps on rising
+					Keep on rising up
+					Uprising, uprising
+					Uprising, uprising
+					Sisters, Brothers, Lovers
+					This girl keep on rising up
+					Sisters, keep on rising
+					Brothers, keep on rising
+					This girl keeps on rising
+					Keep on rising up
+				</p>
+	  		</div>
+	  		<div class="item">
+	  			<h3>Uprising Muse</h3>
+	  			<img src="img/Uprisingmuse.jpg" alt="item">
+	  			<p>
+Paranoia is in bloom
+The PR transmissions will resume
+They'll try to push drugs that keep us all dumbed down
+And hope that we will never see the truth around
+(So come on)
+Another promise, another scene
+Another packaged lie to keep us trapped in greed
+And all the green belts wrapped around our minds
+And endless red tape to keep the truth confined
+(So come on)
+They will not force us
+They will stop degrading us
+They will not control us
+We will be victorious
+</p>
+	  			<button id="readMore5">Lees meer</button><button id="readLess5" style="display: none">Lees minder</button>
+	  			<p id="item5" style="display: none">
+(So come on)
+Interchanging mind control
+Come, let the revolution take its toll
+If you could flick the switch and open your third eye
+You'd see that we should never be afraid to die
+(So come on)
+Rise up and take the power back
+It's time the fat cats had a heart attack
+You know that their time's coming to an end
+We have to unify and watch our flag ascend
+(So come on)
+They will not force us
+They will stop degrading us
+They will not control us
+We will be victorious
+(So come on)
+They will not force us
+They will stop degrading us
+They will not control us
+We will be victorious
+(So come on)</p>
+	  		</div>
+	  		<div class="item">
+	  			<h3>Plug in baby</h3>
+	  			<img src="img/pluginbaby.jpg" alt="item">
+	<p>
+… I've exposed your lies, baby
+The underneath no big surprise
+Now it's time for changing
+And cleansing everything
+To forget your love
+… My plug in baby
+Crucifies my enemies
+When I'm tired of giving
+My plug in baby
+In unbroken virgin realities
+Is tired of living
+… Don't confuse
+Baby you're gonna lose
+Your own game
+Change me
+And replace the envying
+To forget your love
+… My plug in baby
+Crucifies my enemies
+When I'm tired of giving
+My plug in baby
+In unbroken virgin realities
+And tired of living
+And I've seen your loving
+Mine is gone
+</p>
+</div>
+<div class="item">
+	  			<h3>We are the champions </h3>
+	  			<img src="img/champions.jpg" alt="item">
+	  			<p>
+I've paid my dues time after time
+I've done my sentence but committed no crime
+And bad mistakes, I've made a few
+I've had my share of sand kicked in my face
+But I've come through
+And we mean to go on, and on, and on, and on
+We are the champions, my friends
+And we'll keep on fighting till the end
+We are the champions, we are the champions
+No time for losers, 'cause we are the champions of the world
+I've taken my bows and my curtain calls
+You brought me fame and fortune
+
+</p>
+<button id="readMore6">Lees meer</button><button id="readLess6" style="display: none">Lees minder</button>
+	  			<p id="item6" style="display: none">
+				  And everything that goes with it, I thank you all
+But it's been no bed of roses, no pleasure cruise
+I consider it a challenge before
+The whole human race and I ain't gonna lose
+And we mean to go on, and on, and on and on
+We are the champions, my friends
+And we'll keep on fighting till the end
+We are the champions, we are the champions
+No time for losers, 'cause we are the champions of the world
+We are the champions, my friends
+And we'll keep on fighting till the end, oh
+We are the champions, we are the champions
+No time for losers, 'cause we are the champions
+</p>
+</div>
+<div class="item">
+	  			<h3>Uprising Muse</h3>
+	  			<img src="img/Uprisingmuse.jpg" alt="item">
+	  			<p>
+Paranoia is in bloom
+The PR transmissions will resume
+They'll try to push drugs that keep us all dumbed down
+And hope that we will never see the truth around
+(So come on)
+Another promise, another scene
+Another packaged lie to keep us trapped in greed
+And all the green belts wrapped around our minds
+And endless red tape to keep the truth confined
+(So come on)
+They will not force us
+They will stop degrading us
+They will not control us
+We will be victorious
+</p>
+	  			<button id="readMore7">Lees meer</button><button id="readLess7" style="display: none">Lees minder</button>
+	  			<p id="item7" style="display: none">
+(So come on)
+Interchanging mind control
+Come, let the revolution take its toll
+If you could flick the switch and open your third eye
+You'd see that we should never be afraid to die
+(So come on)
+Rise up and take the power back
+It's time the fat cats had a heart attack
+You know that their time's coming to an end
+We have to unify and watch our flag ascend
+(So come on)
+They will not force us
+They will stop degrading us
+They will not control us
+We will be victorious
+(So come on)
+They will not force us
+They will stop degrading us
+They will not control us
+We will be victorious
+(So come on)
+</p>
+</div>
+<div class="item">
+	  			<h3>“Transylvia” van Iron Maiden</h3>
+	  			<img src="img/ironmaiden.jpg" alt="item">
+	  			<p>
+				  (Instrumental)
+</p>
+</div>
+</main>
+<script type="text/javascript">
+	document.getElementById('readMore').addEventListener('click', function(){
+		document.getElementById('item1').style.display = 'block';
+		document.getElementById('readLess').style.display = 'block';
+		document.getElementById('readMore').style.display = 'none';
+	})
+	document.getElementById('readLess').addEventListener('click', function(){
+		document.getElementById('item1').style.display = 'none';
+		document.getElementById('readMore').style.display = 'block';
+		document.getElementById('readLess').style.display = 'none';
+	})
+	document.getElementById('readMore2').addEventListener('click', function(){
+		document.getElementById('item2').style.display = 'block';
+		document.getElementById('readLess2').style.display = 'block';
+		document.getElementById('readMore2').style.display = 'none';
+	})
+	document.getElementById('readLess2').addEventListener('click', function(){
+		document.getElementById('item2').style.display = 'none';
+		document.getElementById('readMore2').style.display = 'block';
+		document.getElementById('readLess2').style.display = 'none';
+	})
+	document.getElementById('readMore3').addEventListener('click', function(){
+		document.getElementById('item3').style.display = 'block';
+		document.getElementById('readLess3').style.display = 'block';
+		document.getElementById('readMore3').style.display = 'none';
+	})
+	document.getElementById('readLess3').addEventListener('click', function(){
+		document.getElementById('item3').style.display = 'none';
+		document.getElementById('readMore3').style.display = 'block';
+		document.getElementById('readLess3').style.display = 'none';
+	})
+	document.getElementById('readMore4').addEventListener('click', function(){
+		document.getElementById('item4').style.display = 'block';
+		document.getElementById('readLess4').style.display = 'block';
+		document.getElementById('readMore4').style.display = 'none';
+	})
+	document.getElementById('readLess4').addEventListener('click', function(){
+		document.getElementById('item4').style.display = 'none';
+		document.getElementById('readMore4').style.display = 'block';
+		document.getElementById('readLess4').style.display = 'none';
+	})
+	document.getElementById('readMore5').addEventListener('click', function(){
+		document.getElementById('item5').style.display = 'block';
+		document.getElementById('readLess5').style.display = 'block';
+		document.getElementById('readMore5').style.display = 'none';
+	})
+	document.getElementById('readLess5').addEventListener('click', function(){
+		document.getElementById('item5').style.display = 'none';
+		document.getElementById('readMore5').style.display = 'block';
+		document.getElementById('readLess5').style.display = 'none';
+	})
+	document.getElementById('readMore6').addEventListener('click', function(){
+		document.getElementById('item6').style.display = 'block';
+		document.getElementById('readLess6').style.display = 'block';
+		document.getElementById('readMore6').style.display = 'none';
+	})
+	document.getElementById('readLess6').addEventListener('click', function(){
+		document.getElementById('item6').style.display = 'none';
+		document.getElementById('readMore6').style.display = 'block';
+		document.getElementById('readLess6').style.display = 'none';
+	})
+	document.getElementById('readMore7').addEventListener('click', function(){
+		document.getElementById('item7').style.display = 'block';
+		document.getElementById('readLess7').style.display = 'block';
+		document.getElementById('readMore7').style.display = 'none';
+	})
+	document.getElementById('readLess7').addEventListener('click', function(){
+		document.getElementById('item7').style.display = 'none';
+		document.getElementById('readMore7').style.display = 'block';
+		document.getElementById('readLess7').style.display = 'none';
+	})
+	document.getElementById('readMore8').addEventListener('click', function(){
+		document.getElementById('item8').style.display = 'block';
+		document.getElementById('readLess8').style.display = 'block';
+		document.getElementById('readMore8').style.display = 'none';
+	})
+	document.getElementById('readLess8').addEventListener('click', function(){
+		document.getElementById('item8').style.display = 'none';
+		document.getElementById('readMore8').style.display = 'block';
+		document.getElementById('readLess8').style.display = 'none';
+	})
+	document.getElementById('readMore9').addEventListener('click', function(){
+		document.getElementById('item9').style.display = 'block';
+		document.getElementById('readLess9').style.display = 'block';
+		document.getElementById('readMore9').style.display = 'none';
+	})
+	document.getElementById('readLess9').addEventListener('click', function(){
+		document.getElementById('item9').style.display = 'none';
+		document.getElementById('readMore9').style.display = 'block';
+		document.getElementById('readLess9').style.display = 'none';
+	})
+</script>
+
+<?php
+require_once("footer.php");
+?>
+</body>
