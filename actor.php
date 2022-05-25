a9e59619874faf3a385a23987b71199f27e24eca
HighdraSC2 <davey.smeekens23@outlook.com>
2022-05-25 11:02:51 +0200

    Created actor page

---
+++ actor.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Actors</title>
+</head>
+<body>
+<?php 
+require_once("header.php");
+?>
+<main>
+	<div class="Wrapper">
+		<h4>Actors </h4>
+		<div class="item">
+			<div class="actor1">
+	  			<h3>Timothy Dalton</h3>
+	  			<img src="img/Timothy.jpg" alt="item">
+	  			<p>	Dalton werd bekend door zijn eerste film, The Lion in Winter, waarin hij de koning van Frankrijk speelde. Omdat Pierce Brosnan door contractuele verplichtingen niet in aanmerking kwam als de opvolger van Roger Moore, werd Dalton gevraagd om de hoofdrol te spelen in de James Bondfilms.
+                   	Hij zette zich er persoonlijk voor in om de Bondfilms in de geest van de boeken van Ian Fleming te maken. Hij las de boeken en zette een Bond neer die sterk overeenkwam met de persoon zoals Fleming die bedacht had. Hij speelde in de The Living Daylights (1987, met Bondmeisje Maryam d'Abo) en in Licence to Kill (1989, Bondmeisjes Talisa Soto en Carey Lowell). Daarna stopte Dalton met Bond, ging hij acteren in de televisieserie Scarlett en had hij een bijrol in de film Hot Fuzz. Hij speelde verder Julius Caesar in de televisiefilm Cleopatra uit 1999.
+				</p>
+			</div>
+		<div class="actor2">
+	  			<h3>DarthVader</h3>
+	  			<img src="img/darthvader.jpg" alt="item">
+	  			<p>	Darth Vader is een persoon uit de Star Wars-films. Hij is door ernstige brandwonden, die hij heeft opgelopen tijdens een gevecht op Mustafar; Star Wars: Episode III, getransformeerd tot een Cyborg. Hij is als Sith-leerling ondergeschikt aan keizer Palpatine, ook bekend als Darth Sidious.
+                   	In de jaren 1977, 1980 en 1983 werd Darth Vader gespeeld door acteur David Prowse en stuntman Bob Anderson. Zijn stem werd echter ingesproken door James Earl Jones, omdat diens stem veel zwaarder, duisterder en dreigender klonk.
+                   	Darth Vader is een dienaar van de kwaadaardige keizer Palpatine (Darth Sidious) en een wreed persoon. Hij is vooral te herkennen aan zijn donkere kostuum, zijn zware stem, en zijn enorme omvang. Hij wordt zowel gevreesd door de Rebellen, als door zijn eigen troepen.
+				</p>
+		 	</div>
+	  	<div class="actor3">
+	  			<h3>Luke Skywalker</h3>
+	  			<img src="img/LukeSkywalker.png" alt="item">
+	  			<p>	Luke Skywalker is een personage uit de Star Warssaga, gespeeld door acteur Mark Hamill. Luke is de tweelingbroer van prinses Leia. Hun ouders zijn Padmé Amidala en Anakin Skywalker.
+                	Skywalker werd opgevoed door zijn oom Owen Lars en tante Beru Whitesun Lars.	
+				</p>
+			</div>
+	</div>
+</main>
+
+<?php
+require_once("footer.php");
+?>
+</body>
+</body>
+</html>
