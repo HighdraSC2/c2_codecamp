97dfb65f568ad7dfab0cbb6a2930a66075d9085d
HighdraSC2 <davey.smeekens23@outlook.com>
2022-05-25 11:01:49 +0200

    Created films page

---
+++ films.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Films</title>
+</head>
+<body>
+<?php 
+require_once("header.php");
+?>
+<main>
+	<div class="Wrapper">
+		<h4>James bond films van Roger Moore </h4>
+		<div class="item">
+			<div class="film1">
+	  			<h3>Live and let it die (1973)</h3>
+	  			<img src="img/liveanddie.jpg" alt="item">
+	  			<p>Binnen één etmaal worden drie Britse agenten vermoord: de eerste in New York, in het hoofdkwartier van de VN, waar hij Dr. Kananga, de leider van het Caribische eiland San Monique, in de gaten hield;
+					   de tweede in New Orleans die het restaurant "Fillet of Soul" in de gaten houdt;
+					    de derde op San Monique zelf. Er is dus mogelijk een connectie, en James Bond wordt op missie naar New York gestuurd, waar hij zal samenwerken met Felix Leiter van de CIA, die Kananga's ambassade afluistert.
+						Direct bij aankomst in New York wordt de chauffeur van Bond vermoord. Bond volgt de moordenaar naar een winkel vol voodoo-items.
+					    Vanuit de winkel volgt hij een auto, die naar Harlem gaat, en stopt bij een restaurant dat ook "Filet of Soul" heet. In het restaurant wordt Bond opgewacht en overmeesterd.
+					    Daar ontmoet hij Solitaire, een helderziend meisje dat voor Kananga werkt, en gangsterleider Mr. Big, die terstond opdracht geeft om Bond te doden.
+						Buiten weet Bond te ontsnappen, met behulp van CIA-agent Harold Strutter. Deze legt hem uit dat Mr. Big de machtigste crimineel van de stad is. De vraag is wat Kananga en hij met elkaar te maken hebben.</p>
+			</div>
+
+			<div class="film2">
+	  			<h3>The man with the golden gun (1974)</h3>
+	  			<img src="img/goldengun.jpg" alt="item">
+	  			<p>An American gangster, Rodney, visits famed crack shot hitman Francisco Scaramanga to kill him and collect a bounty, but he is directed into a funhouse section of the estate where there are mannequins of gangsters.
+				   Scaramanga eventually retrieves his golden gun and kills the gangster.
+                   In London, a golden bullet etched with '007' is received by MI6; it is believed to have been sent by Scaramanga, but because no one knows of his appearance outside of having a third nipple,
+				   M relieves Bond of his current mission involving tracking an energy scientist named Gibson.</p>
+			</div>
+
+			<div class="film3">
+					<h3>The spy who loved me (1977)</h3>
+					<img src="img/thespy.jpg" alt="item">
+					<p>A British and a Soviet ballistic-missile submarine suddenly disappear. James Bond – MI6 agent 007 – is summoned to investigate. On the way to his briefing, Bond escapes an ambush by a squad of Soviet agents in Austria,
+					killing one during a downhill ski chase and evading the others. The plans for a highly advanced submarine tracking system are being offered in Egypt. There, Bond encounters Major Anya Amasova—KGB agent Triple X—as a rival for the microfilm plans.</p>
+		    </div>
+			<div class="film4">
+					<h3>Moonraker (1979</h3>
+					<img src="img/moonraker.jpg" alt="item">
+					<p>A Drax Industries Space Shuttle, Moonraker — on loan to the United Kingdom — is hijacked in midair while on a Shuttle Carrier Aircraft. The carrier is destroyed but no wreckage of the Shuttle is found. M, head of MI6, assigns James Bond, Agent 007, to investigate.
+					En route to England, Bond is attacked and pushed out of an aeroplane by the mercenary assassin Jaws (whom he previously met in The Spy Who Loved Me). He survives by stealing a parachute from the pilot, while Jaws lands on a trapeze net within a circus tent.</p>
+			</div>
+			<div class="film5">
+					<h3>For your eyes only (1981)</h3>
+					<img src="img/youreyes.jpeg" alt="item">
+					<p>The British information gathering vessel St Georges, which holds the Automatic Targeting Attack Communicator (ATAC), the system used by the Ministry of Defence to co-ordinate the Royal Navy's fleet of Polaris submarines,
+					is sunk after accidentally trawling an old naval mine in the Ionian Sea. A marine archaeologist, Sir Timothy Havelock, is asked by the British to secretly locate the St Georges. However, he and his wife are murdered by a Cuban hitman, Hector Gonzales. His daughter, Melina Havelock, witnesses the murder and vows revenge.</p>
+			</div>
+			<div class="film6">
+					<h3>Octopussy (1983)</h3>
+					<img src="img/Octopussy.jpg" alt="item">
+					<p>After fleeing knife-throwing twin assassins Mischka and Grishka in East Berlin, mortally wounded British agent 009, dressed as a circus clown and carrying a counterfeit Fabergé egg, crashes into the British ambassador's residence and dies.
+					MI6 immediately suspects Soviet involvement and, after the genuine Fabergé egg is to be auctioned in London, sends James Bond to identify the seller. At the auction,</p>
+			</div>
+			<div class="film7">
+					<h3> A view to a kill (1985)</h3>
+					<img src="img/viewtokill_.jpg" alt="item">
+					<p>Bond is in Siberië op zoek naar het dode lichaam van agent 003 en een uit de Sovjet-Unie afkomstige microchip die deze op zak heeft. Later blijkt dat een exacte kopie te zijn van een microchip van 'Zorin Industries'.
+					Bond moet de onverklaarbare dood van agent 003 onderzoeken en diens werk afmaken. Zorin Industries is zijn enige aanknopingspunt.</p>
+			</div>
+</main>
+
+<?php
+require_once("footer.php");
+?>
+</body>
+</body>
+</html>
