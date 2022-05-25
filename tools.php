ea8fd08e48bc166a1a18323bc410e91dc9270851
HighdraSC2 <davey.smeekens23@outlook.com>
2022-05-25 10:59:57 +0200

    Created tools page

---
+++ tools.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Tools</title>
+</head>
+<body>
+<?php 
+require_once("header.php");
+?>
+<main>
+	<div class="Wrapper">
+		<h4>Een lijst van tools </h4>
+		<div class="item">
+			<div class="tool1">
+	  			<h3>Cirkelzaag</h3>
+	  			<img src="img/cirkelzaag.jpg" alt="item">
+	  			<p>Een cirkelzaag (of radiaalzaag) is een zaag met een blad in de vorm van een stalen schijf, die met een motor tot een zeer hoog toerental wordt aangedreven. De schijf kan in een zaagtafel zijn gemonteerd waarover het te zagen materiaal wordt aangevoerd, of ingebouwd in een constructie samen met een elektromotor tot draagbare handcirkelzaag. Deze heeft een minder krachtige motor en kan ook dunner materiaal zagen.
+                   Cirkelzaagbladen bestaan in soorten en maten, met allerlei verschillende vertandingen, en ook de diameter kan sterk verschillen (normaal is ca. 25 cm, maar industriële cirkelzagen met veel grotere bladen bestaan ook). De maximale zaagdiepte wordt bepaald door de straal van het cirkelzaagblad.
+                   Een cirkelzaag die bedoeld is om latten, buizen, houten balken en dergelijke te zagen wordt ook wel een afkorter of afkortzaag genoemd.
+</p>
+
+		    <div class="tool2">
+	  			<h3>Kettingzaag</h3>
+	  			<img src="img/kettingzaag.jpg" alt="item">
+	  			<p>Een kettingzaag bestaat uit drie delen:
+                   1.	Motor: de benzinemotor moet gestart worden met een startkoord (als bij een motormaaier). De motor drijft een kettingrondsel aan, dat zorgt dat de ketting gaat ronddraaien. Bij een elektrische kettingzaag dient men op een knop te drukken om de motor te starten.
+                   2.	Zaagblad: het zaagblad of "zwaard" zit aan de motorbehuizing vastgemaakt. Het zaagblad heeft over de hele lengte een groef. De functie van de groef is tweeledig. Enerzijds zorgt de groef ervoor dat de ketting rechtuit blijft lopen. Anderzijds zorgt de groef voor vervoer van olie, die de ketting smeert. De groef moet voldoende diep zijn, en vrij zijn van vuil en zand. Dit alles om de ketting er niet uit te laten lopen. Het blad mag niet beschadigd zijn en er mogen geen bramen op zitten.
+                   3.	Zaagketting: een schakelketting met zaagtanden, ook wel beitels genoemd. Een zaagketting bestaat uit drie onderdelen: de zaagtand, de dieptesteller en de aandrijfschakel. Deze zijn met elkaar verbonden door verbindingsschakels. De zaagtand doet het werk. Deze beitelt het hout uit de zaagsnede. De dieptesteller bepaalt de diepte waarmee de zaagtand door het hout gaat. Staat deze te hoog afgesteld neemt de zaagtand bijna geen hout mee. Staat deze te diep afgesteld kan de zaagtand vastlopen in het hout (ook wel 'agressief' genoemd). De aandrijfschakel loopt door de groef van het zaagblad. Hieruit haalt hij olie om de ketting te smeren.
+</p>
+		<div class="item">
+			<div class="tool3">
+	  			<h3>Hamer</h3>
+	  			<img src="img/hamer.jpg" alt="item">
+	  			<p>Een hamer is een klop- of slagwerktuig. Een hamer bestaat uit een steel, het handvat, met aan het eind de zogenaamde hamerkop.
+				   De vorm en grootte en het materiaal van de hamerkop en de lengte van de steel hangen sterk af van het type hamer. Bij hamers met een ijzeren hamerkop staat vaak het aantal grammen in het metaal geslagen. Voor speciale uitvoeringen, zoals voor percussie-instrumenten, wordt ook de term mallet (uit het Engels) gebruikt.
+</p>
+</main>
+
+<?php
+require_once("footer.php");
+?>
+</body>
+</body>
+</html>
